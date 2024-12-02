0aa665d278f7a49b28160c9dd9ce961910e93879
copter7788 <copterr7788@gmail.com>
2024-12-02 15:10:49 +0900

    add hw4.1

--- HW4/MathMethods.java
+++ HW4/MathMethods.java
@@ -1,24 +1,19 @@
 public class MathMethods {
-    // Method for the first calculation: √(x^2 + y^2)
     public static double calculateFirst(double x, double y) {
         return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
     }
 
-    // Method for the second calculation: x(1 + |sin(x)| + |cos(x)|)
     public static double calculateSecond(double x) {
         return x * (1 + Math.abs(Math.sin(x)) + Math.abs(Math.cos(x)));
     }
 
     public static void main(String[] args) {
-        // Given x = 5, y = 4
         double x = 5;
         double y = 4;
 
-        // Calculate results using the methods
         double result1 = calculateFirst(x, y);
         double result2 = calculateSecond(x);
 
-        // Print the results
         System.out.println("Result of first calculation: " + result1);
         System.out.println("Result of second calculation: " + result2);
     }
